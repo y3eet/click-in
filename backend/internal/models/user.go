bac579e463e10e638df1c9c7fd135555c1bff01c
y3eet <jcsernicula9@gmail.com>
2025-11-23 20:50:22 +0800

    feat: update user model

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -7,11 +7,13 @@ import (
 )
 
 type User struct {
-	ID        uint           `gorm:"primarykey" json:"id"`
-	Email     string         `gorm:"uniqueIndex;not null" json:"email"`
-	Username  string         `json:"username"`
-	AvatarURL string         `json:"avatar_url"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+	ID         uint           `gorm:"primarykey" json:"id"`
+	ProviderID string         `gorm:"uniqueIndex;not null" json:"provider_id"`
+	Email      string         `gorm:"uniqueIndex;not null" json:"email"`
+	Username   string         `json:"username"`
+	AvatarURL  string         `json:"avatar_url"`
+	Provider   string         `json:"provider"`
+	CreatedAt  time.Time      `json:"created_at"`
+	UpdatedAt  time.Time      `json:"updated_at"`
+	DeletedAt  gorm.DeletedAt `gorm:"index" json:"-"`
 }
