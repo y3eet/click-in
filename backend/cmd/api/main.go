fe1facb88d901e800ae23c973fe3d4aa922522d2
Jc <107809331+y3eet@users.noreply.github.com>
2025-11-28 08:12:36 +0800

    feat: add cors

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/y3eet/click-in/internal/auth"
 	"github.com/y3eet/click-in/internal/config"
@@ -20,6 +22,16 @@ func main() {
 	}
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", cfg.FrontendURL}, // frontend URL
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	routes.SetupRoutes(r, db, cfg)
 
 	if err := r.Run(":" + cfg.Port); err != nil {
--- backend/go.mod
+++ backend/go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/bytedance/sonic/loader v0.4.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.11 // indirect
+	github.com/gin-contrib/cors v1.7.6 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-chi/chi/v5 v5.2.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
