caaa7ca8872452ef9336aef7a36f9dcc80be940d
Jc <107809331+y3eet@users.noreply.github.com>
2025-12-04 08:59:28 +0800

    fix: container height

--- web/src/app/user/layout.tsx
+++ web/src/app/user/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   return (
     <div>
       <Topbar />
-      <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8 h-full">
         {children}
       </div>
     </div>
