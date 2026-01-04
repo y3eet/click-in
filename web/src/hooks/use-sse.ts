838fc01afeff91dd16da9f3a688f8d1438bf84da
y3eet <jcsernicula9@gmail.com>
2026-01-04 12:07:59 +0800

    feat: remove log

--- web/src/hooks/use-sse.ts
+++ web/src/hooks/use-sse.ts
@@ -25,7 +25,6 @@ export function useSSE<T>(
     };
 
     eventSource.onerror = (error: Event) => {
-      console.error("SSE error:", error);
       onError(error);
       eventSource.close();
     };
