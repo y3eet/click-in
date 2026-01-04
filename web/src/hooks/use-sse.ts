a6c9157c7d651a32e200951bbe4035a7690576e5
y3eet <jcsernicula9@gmail.com>
2026-01-04 12:02:25 +0800

    feat: create use sse hook

---
+++ web/src/hooks/use-sse.ts
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+
+type SSEResponse<T> = T & {
+  data: T;
+  timestamp: string;
+};
+
+export function useSSE<T>(
+  url: string,
+  onMessage: (data: SSEResponse<T>) => void,
+  onError: (error: Event) => void
+) {
+  useEffect(() => {
+    const resolvedUrl = process.env.NEXT_PUBLIC_API_URL + url;
+    const eventSource = new EventSource(resolvedUrl, { withCredentials: true });
+
+    eventSource.onmessage = (event) => {
+      try {
+        const data: SSEResponse<T> = JSON.parse(event.data);
+        onMessage(data);
+      } catch (error: unknown) {
+        console.log("Error parsing SSE message:", error);
+        onMessage(event.data);
+      }
+    };
+
+    eventSource.onerror = (error: Event) => {
+      console.error("SSE error:", error);
+      onError(error);
+      eventSource.close();
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, [url, onMessage, onError]);
+}
