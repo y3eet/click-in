46ae0524587179b32995ac32312a5e571b90731b
y3eet <jcsernicula9@gmail.com>
2026-02-21 17:22:26 +0800

    fix: backend url

--- web/src/lib/config.ts
+++ web/src/lib/config.ts
@@ -1,5 +1,4 @@
-// const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "";
-const apiUrl = "https://click-api.y3eet.me";
+const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "";
 
 if (!apiUrl) {
   console.warn("Environment variable NEXT_PUBLIC_API_URL is not set.");
